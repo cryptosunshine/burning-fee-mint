aac1336b9f09fa71fd5224ca0579f7ca2cb8039a
cryptosunshine <shouhui.sun@gmail.com>
2022-06-13 11:51:06 +0800

    fix proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,24 +3,27 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
 
     // 测试节点
-    createProxyMiddleware({
-        target: 'https://relay-goerli.flashbots.net/',
-        changeOrigin: true,
-        pathRewrite: {
-            // 路径重写，去掉xxx路径
-            '^/flashbots_test_cors': '/'
-        }
-    })(req, res);
+    if (req.url.startsWith('/flashbots_test_cors')) {
+        createProxyMiddleware({
+            target: 'https://relay-goerli.flashbots.net/',
+            changeOrigin: true,
+            pathRewrite: {
+                // 路径重写，去掉xxx路径
+                '^/flashbots_test_cors': '/'
+            }
+        })(req, res);
+    }
 
     // 主网
-    createProxyMiddleware({
-      target: 'https://relay.flashbots.net/',
-      changeOrigin: true,
-      pathRewrite: {
-          // 路径重写，去掉xxx路径
-          '^/flashbots_cors': '/'
-      }
-    })(req, res);
+    if (req.url.startsWith('/flashbots_cors')) {
+        createProxyMiddleware({
+            target: 'https://relay.flashbots.net/',
+            changeOrigin: true,
+            pathRewrite: {
+                // 路径重写，去掉xxx路径
+                '^/flashbots_cors': '/'
+            }
+        })(req, res);
+    }
 
-    
 }
